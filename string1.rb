56d44f2409cd1b582ce42e81df25ae85d10af924
Michael Jacob Manwaring <manwaringmj@dcsdk12.org>
2016-09-22 14:42:36 -0600

    the make outer is done and the first half needs to be fixed

--- string1.rb
+++ string1.rb
@@ -2,4 +2,17 @@ def helloName(str)
   return "Hello " + str
 end
 
-print helloName("Sammmmmmmm")
+# print helloName("Sammmmmmmm")
+
+
+def makeOutWorld(a,word,c)
+  return a + word + c
+end
+
+# print makeOutWorld("<<", "word", ">>")
+
+def firstHalf(a,b)
+  return a
+end
+
+print firstHalf("Woo","Hoo")
